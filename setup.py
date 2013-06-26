aac1aa0de27deb88a3984965675eac7218b56087
Nando Florestan <nandoflorestan@gmail.com>
2013-06-26 16:17:50 -0300

    Next release shall include the file porting.rst

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     url='https://github.com/nandoflorestan/nine',
     name="nine",
     author='Nando Florestan',
-    version='0.1.0',
+    version='0.1.1dev',
     license='Public domain',
     packages=find_packages(),
     include_package_data=True,
