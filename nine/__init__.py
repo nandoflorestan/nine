a1183e97fd5fa49413da72d147fbba21df3fecc1
Nando Florestan <nandoflorestan@gmail.com>
2020-01-22 08:00:29 +0100

    Format Python code with black

--- nine/__init__.py
+++ nine/__init__.py
@@ -8,8 +8,8 @@ This module is donated to the public domain.
 import sys
 
 # Test for Python 2, not 3; don't get bitten when Python 4 appears:
-IS_PYTHON2 = (sys.version_info[0] == 2)
-IS_PYPY = hasattr(sys, 'pypy_translation_info')
+IS_PYTHON2 = sys.version_info[0] == 2
+IS_PYPY = hasattr(sys, "pypy_translation_info")
 from importlib import import_module
 
 if IS_PYTHON2:  # Rename Python 2 builtins so they become like Python 3
@@ -21,6 +21,7 @@ if IS_PYTHON2:  # Rename Python 2 builtins so they become like Python 3
     integer_types = (int, long)
     long = long
     from types import ClassType
+
     class_types = (type, ClassType)
     del ClassType
     range_list = range
@@ -29,6 +30,7 @@ if IS_PYTHON2:  # Rename Python 2 builtins so they become like Python 3
     itervalues = lambda d: d.itervalues()
     iteritems = lambda d: d.iteritems()
     from itertools import ifilter as filter, imap as map, izip as zip
+
     # In Python 2, *input* was equivalent to eval(raw_input(prompt)):
     input = raw_input
 else:  # For Python 3, declare these variables so they can be chain imported:
@@ -49,15 +51,18 @@ else:  # For Python 3, declare these variables so they can be chain imported:
 
 if IS_PYTHON2:
     # Turn code into string to avoid SyntaxError on Python 3:
-    exec('def reraise(tp, value, tb=None):\n  raise tp, value, tb')
+    exec("def reraise(tp, value, tb=None):\n  raise tp, value, tb")
 else:
+
     def reraise(tp, value, tb=None):
         if value.__traceback__ is not tb:
             raise value.with_traceback(tb)
         raise value
 
+
 # ===== Class decorators =====
 if IS_PYTHON2:
+
     def implements_to_string(cls):
         """Class decorator that converts __str__() and __bytes__.
 
@@ -68,8 +73,11 @@ if IS_PYTHON2:
         __unicode__() and encodes the result to utf-8.
         """
         cls.__unicode__ = cls.__str__
-        cls.__str__ = cls.__bytes__ if hasattr(cls, '__bytes__') \
-            else lambda x: x.__unicode__().encode('utf-8')
+        cls.__str__ = (
+            cls.__bytes__
+            if hasattr(cls, "__bytes__")
+            else lambda x: x.__unicode__().encode("utf-8")
+        )
         return cls
 
     def implements_iterator(cls):
@@ -87,7 +95,8 @@ if IS_PYTHON2:
         cls.__repr_unicode__ = cls.__repr__
 
         def wrapper(self):
-            return self.__repr_unicode__().encode('utf-8')
+            return self.__repr_unicode__().encode("utf-8")
+
         cls.__repr__ = wrapper
         return cls
 
@@ -104,89 +113,88 @@ if IS_PYTHON2:
 
         (*nine* is all the above class decorators in one.)
         """
-        if hasattr(cls, '__str__'):
+        if hasattr(cls, "__str__"):
             cls = implements_to_string(cls)
-        if hasattr(cls, '__next__'):
+        if hasattr(cls, "__next__"):
             cls = implements_iterator(cls)
-        if hasattr(cls, '__repr__'):
+        if hasattr(cls, "__repr__"):
             cls = implements_repr(cls)
         return cls
+
+
 else:  # On Python 3, these class decorators do nothing:
-    implements_to_string = implements_iterator = implements_repr = nine = \
-        lambda cls: cls
+    implements_to_string = (
+        implements_iterator
+    ) = implements_repr = nine = lambda cls: cls
 
 
 # http://docs.pythonsprints.com/python3_porting/py-porting.html
 _moved = {  # Mapping from Python 3 to Python 2 location. May need improvement.
-    'builtins': '__builtin__',
-    'configparser': 'ConfigParser',
-    'copyreg': 'copy_reg',
-    '_markupbase': 'markupbase',
-    'pickle': 'cPickle',
-    'queue': 'Queue',
-    'reprlib': 'repr',
-    'socketserver': 'SocketServer',
-    '_thread': 'thread',
-    'tkinter': 'Tkinter',
-    'http.client':    'httplib',
-    'http.cookiejar': 'cookielib',
-    'http.cookies':   'Cookie',
-    'html.entities':                'htmlentitydefs',
-    'html.entities:entitydefs':     'htmlentitydefs:entitydefs',
-    'html.entities:name2codepoint': 'htmlentitydefs:name2codepoint',
-    'html.entities:codepoint2name': 'htmlentitydefs:codepoint2name',
-    'html:escape': 'cgi:escape',
-    'html.parser:HTMLParser': 'htmllib:HTMLParser',
-    'urllib.robotparser': 'robotparser',
-    'urllib.error:ContentTooShortError': 'urllib:ContentTooShortError',
-    'urllib.parse':              'urlparse',
-    'urllib.parse:quote':        'urllib:quote',
-    'urllib.parse:quote_plus':   'urllib:quote_plus',
-    'urllib.parse:unquote':      'urllib:unquote',
-    'urllib.parse:unquote_plus': 'urllib:unquote_plus',
-    'urllib.parse:urlencode':    'urllib:urlencode',
-    'urllib.request:getproxies':     'urllib:getproxies',
-    'urllib.request:pathname2url':   'urllib:pathname2url',
-    'urllib.request:url2pathname':   'urllib:url2pathname',
-    'urllib.request:urlcleanup':     'urllib:urlcleanup',
-    'urllib.request:urlretrieve':    'urllib:urlretrieve',
-    'urllib.request:URLopener':      'urllib:URLopener',
-    'urllib.request:FancyURLopener': 'urllib:FancyURLopener',
-    'urllib.request:urlopen':                'urllib2:urlopen',
-    'urllib.request:install_opener':         'urllib2:install_opener',
-    'urllib.request:build_opener':           'urllib2:build_opener',
-    'urllib.error:URLError':                 'urllib2:URLError',
-    'urllib.error:HTTPError':                'urllib2:HTTPError',
-    'urllib.request:Request':                'urllib2:Request',
-    'urllib.request:OpenerDirector':         'urllib2:OpenerDirector',
-    'urllib.request:BaseHandler':            'urllib2:BaseHandler',
-    'urllib.request:HTTPDefaultErrorHandler':
-    'urllib2:HTTPDefaultErrorHandler',
-    'urllib.request:HTTPRedirectHandler':    'urllib2:HTTPRedirectHandler',
-    'urllib.request:HTTPCookieProcessor':    'urllib2:HTTPCookieProcessor',
-    'urllib.request:ProxyHandler':           'urllib2:ProxyHandler',
-    'urllib.request:HTTPPasswordMgr':        'urllib2:HTTPPasswordMgr',
-    'urllib.request:HTTPPasswordMgrWithDefaultRealm':
-    'urllib2:HTTPPasswordMgrWithDefaultRealm',
-    'urllib.request:AbstractBasicAuthHandler':
-    'urllib2:AbstractBasicAuthHandler',
-    'urllib.request:HTTPBasicAuthHandler':   'urllib2:HTTPBasicAuthHandler',
-    'urllib.request:ProxyBasicAuthHandler':  'urllib2:ProxyBasicAuthHandler',
-    'urllib.request:AbstractDigestAuthHandler':
-    'urllib2:AbstractDigestAuthHandler',
-    'urllib.request:HTTPDigestAuthHandler':  'urllib2:HTTPDigestAuthHandler',
-    'urllib.request:ProxyDigestAuthHandler': 'urllib2:ProxyDigestAuthHandler',
-    'urllib.request:HTTPHandler':            'urllib2:HTTPHandler',
-    'urllib.request:HTTPSHandler':           'urllib2:HTTPSHandler',
-    'urllib.request:FileHandler':            'urllib2:FileHandler',
-    'urllib.request:FTPHandler':             'urllib2:FTPHandler',
-    'urllib.request:CacheFTPHandler':        'urllib2:CacheFTPHandler',
-    'urllib.request:UnknownHandler':         'urllib2:UnknownHandler',
+    "builtins": "__builtin__",
+    "configparser": "ConfigParser",
+    "copyreg": "copy_reg",
+    "_markupbase": "markupbase",
+    "pickle": "cPickle",
+    "queue": "Queue",
+    "reprlib": "repr",
+    "socketserver": "SocketServer",
+    "_thread": "thread",
+    "tkinter": "Tkinter",
+    "http.client": "httplib",
+    "http.cookiejar": "cookielib",
+    "http.cookies": "Cookie",
+    "html.entities": "htmlentitydefs",
+    "html.entities:entitydefs": "htmlentitydefs:entitydefs",
+    "html.entities:name2codepoint": "htmlentitydefs:name2codepoint",
+    "html.entities:codepoint2name": "htmlentitydefs:codepoint2name",
+    "html:escape": "cgi:escape",
+    "html.parser:HTMLParser": "htmllib:HTMLParser",
+    "urllib.robotparser": "robotparser",
+    "urllib.error:ContentTooShortError": "urllib:ContentTooShortError",
+    "urllib.parse": "urlparse",
+    "urllib.parse:quote": "urllib:quote",
+    "urllib.parse:quote_plus": "urllib:quote_plus",
+    "urllib.parse:unquote": "urllib:unquote",
+    "urllib.parse:unquote_plus": "urllib:unquote_plus",
+    "urllib.parse:urlencode": "urllib:urlencode",
+    "urllib.request:getproxies": "urllib:getproxies",
+    "urllib.request:pathname2url": "urllib:pathname2url",
+    "urllib.request:url2pathname": "urllib:url2pathname",
+    "urllib.request:urlcleanup": "urllib:urlcleanup",
+    "urllib.request:urlretrieve": "urllib:urlretrieve",
+    "urllib.request:URLopener": "urllib:URLopener",
+    "urllib.request:FancyURLopener": "urllib:FancyURLopener",
+    "urllib.request:urlopen": "urllib2:urlopen",
+    "urllib.request:install_opener": "urllib2:install_opener",
+    "urllib.request:build_opener": "urllib2:build_opener",
+    "urllib.error:URLError": "urllib2:URLError",
+    "urllib.error:HTTPError": "urllib2:HTTPError",
+    "urllib.request:Request": "urllib2:Request",
+    "urllib.request:OpenerDirector": "urllib2:OpenerDirector",
+    "urllib.request:BaseHandler": "urllib2:BaseHandler",
+    "urllib.request:HTTPDefaultErrorHandler": "urllib2:HTTPDefaultErrorHandler",
+    "urllib.request:HTTPRedirectHandler": "urllib2:HTTPRedirectHandler",
+    "urllib.request:HTTPCookieProcessor": "urllib2:HTTPCookieProcessor",
+    "urllib.request:ProxyHandler": "urllib2:ProxyHandler",
+    "urllib.request:HTTPPasswordMgr": "urllib2:HTTPPasswordMgr",
+    "urllib.request:HTTPPasswordMgrWithDefaultRealm": "urllib2:HTTPPasswordMgrWithDefaultRealm",
+    "urllib.request:AbstractBasicAuthHandler": "urllib2:AbstractBasicAuthHandler",
+    "urllib.request:HTTPBasicAuthHandler": "urllib2:HTTPBasicAuthHandler",
+    "urllib.request:ProxyBasicAuthHandler": "urllib2:ProxyBasicAuthHandler",
+    "urllib.request:AbstractDigestAuthHandler": "urllib2:AbstractDigestAuthHandler",
+    "urllib.request:HTTPDigestAuthHandler": "urllib2:HTTPDigestAuthHandler",
+    "urllib.request:ProxyDigestAuthHandler": "urllib2:ProxyDigestAuthHandler",
+    "urllib.request:HTTPHandler": "urllib2:HTTPHandler",
+    "urllib.request:HTTPSHandler": "urllib2:HTTPSHandler",
+    "urllib.request:FileHandler": "urllib2:FileHandler",
+    "urllib.request:FTPHandler": "urllib2:FTPHandler",
+    "urllib.request:CacheFTPHandler": "urllib2:CacheFTPHandler",
+    "urllib.request:UnknownHandler": "urllib2:UnknownHandler",
 }
 if sys.version_info < (3, 9):
     # dummy_thread has been removed from Python 3.9
     # https://bugs.python.org/issue37312
-    _moved['_dummy_thread'] = 'dummy_thread'
+    _moved["_dummy_thread"] = "dummy_thread"
 
 
 def nimport(spec):
@@ -205,11 +213,12 @@ def nimport(spec):
     assert spec
     if IS_PYTHON2:  # Get the Python 2 location of the name, first.
         spec = _moved.get(spec, spec)
-    alist = spec.split(':')
+    alist = spec.split(":")
     if len(alist) > 2:
         raise ValueError(
-            'The argument *spec* cannot have more than '
-            '2 colon-separated parts: "{}"'.format(spec))
+            "The argument *spec* cannot have more than "
+            '2 colon-separated parts: "{}"'.format(spec)
+        )
     elif len(alist) == 2:
         module, name = alist
     elif len(alist) == 1:
@@ -218,5 +227,6 @@ def nimport(spec):
     module = import_module(module)
     return getattr(module, name) if name else module
 
-# don't export nine.six
+
+# don't export nine.sys
 del sys
--- nine/test_nine.py
+++ nine/test_nine.py
@@ -1,22 +1,44 @@
 # -*- coding: utf-8 -*-
 
-'''This module is donated to the public domain.'''
+"""This module is donated to the public domain."""
 
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
 import unittest
 
 
-class TestNine(unittest.TestCase):
+class TestNine(unittest.TestCase):  # noqa
+    def test_import(self):  # noqa
+        from nine import (  # noqa
+            IS_PYTHON2,
+            str,
+            basestring,
+            native_str,
+            integer_types,
+            class_types,
+            range,
+            range_list,
+            reraise,
+            iterkeys,
+            itervalues,
+            iteritems,
+            map,
+            zip,
+            filter,
+            input,
+            implements_iterator,
+            implements_to_string,
+            implements_repr,
+            nine,
+            nimport,
+            _moved,
+        )
 
-    def test_import(self):
-        from nine import (
-            IS_PYTHON2, str, basestring, native_str,
-            integer_types, class_types, range, range_list, reraise,
-            iterkeys, itervalues, iteritems, map, zip, filter, input,
-            implements_iterator, implements_to_string, implements_repr, nine,
-            nimport, _moved)
         for key in _moved:
-            if key == 'tkinter':
+            if key == "tkinter":
                 continue  # travis does not have tk installed :p
             assert nimport(key)
